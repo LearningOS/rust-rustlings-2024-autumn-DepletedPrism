09ff8d65b76c6e8c3f11584d1ff25e87a6f0e450
DepletedPrism <depletedprism@qq.com>
2024-10-03 22:04:50 +0800

    Complete `quiz3`

--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -14,14 +14,26 @@
 // Execute `rustlings hint hashmaps3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 use std::collections::HashMap;
 
 // A structure to store the goal details of a team.
 struct Team {
     goals_scored: u8,
     goals_conceded: u8,
+}
+
+fn append_scores(scores: &mut HashMap<String, Team>, team_name: &str, scored: u8, conceded: u8) {
+    if !scores.contains_key(team_name) {
+        scores.insert(
+            team_name.to_string(),
+            Team {
+                goals_scored: 0,
+                goals_conceded: 0,
+            },
+        );
+    }
+    scores.get_mut(team_name).unwrap().goals_scored += scored;
+    scores.get_mut(team_name).unwrap().goals_conceded += conceded;
 }
 
 fn build_scores_table(results: String) -> HashMap<String, Team> {
@@ -39,6 +51,8 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         // will be the number of goals conceded from team_2, and similarly
         // goals scored by team_2 will be the number of goals conceded by
         // team_1.
+        append_scores(&mut scores, &team_1_name, team_1_score, team_2_score);
+        append_scores(&mut scores, &team_2_name, team_2_score, team_1_score);
     }
     scores
 }
